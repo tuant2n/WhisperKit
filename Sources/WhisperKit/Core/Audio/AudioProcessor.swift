4673163e04b394b6569e19f81fb796fde5f683cb
Tuan Trinh <TuanTrinh@Polymath>
2025-04-22 11:17:21 +0700

    fix

--- Sources/WhisperKit/Core/Audio/AudioProcessor.swift
+++ Sources/WhisperKit/Core/Audio/AudioProcessor.swift
@@ -839,7 +839,17 @@ public extension AudioProcessor {
         }
         
         if let outputFile = outputFile {
-            let audioFile = try AVAudioFile.init(forWriting: outputFile, settings: nodeFormat.settings)
+            let settings = [
+                        AVFormatIDKey: Int(kAudioFormatLinearPCM),
+                        AVSampleRateKey: inputFormat.sampleRate,
+                        AVNumberOfChannelsKey: inputFormat.channelCount,
+                        AVLinearPCMBitDepthKey: 16,
+                        AVLinearPCMIsFloatKey: false,
+                        AVLinearPCMIsBigEndianKey: false,
+                        AVLinearPCMIsNonInterleaved: false
+                    ] as [String: Any]
+            
+            let audioFile = try AVAudioFile.init(forWriting: outputFile, settings: settings)
             self.recordFile = audioFile
         }
 
